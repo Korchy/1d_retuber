79b5d7e7175b9c74aaf28e08203618d50fad6477
Nikita Akimov <korchiy@yandex.ru>
2018-06-20 14:54:31 +0300

    update

--- retuber.py
+++ retuber.py
@@ -26,25 +26,22 @@ bl_info = {
 
 import bpy
 import bmesh
-
+import copy
 
 class Retuber:
 
     arrange_vert_selection = True
+    environment = {
+        'mesh_select_mode': None
+    }
 
     @staticmethod
-    def get_parallel_loops(mesh):
-        bpy.ops.object.mode_set(mode='OBJECT')
-
+    def get_perpendicular_loops(bm):
         loops = []
-        bm = bmesh.new()
-        bm.from_mesh(mesh.data)
-
         bm.edges.ensure_lookup_table()
         selected_edges = [edge for edge in bm.edges if edge.select]
         for edge in selected_edges:
             edge.tag = True
-
         bm.verts.ensure_lookup_table()
         selected_verts = [vert for vert in bm.verts if vert.select]
         # --- may be: to arange vertexes from one extreme and so on
@@ -68,42 +65,39 @@ class Retuber:
                     while not next_loop.edge.tag:
                         next_loop.edge.tag = True
                         vert_loop.append(next_loop.edge.index)
+                        # vert_loop.append(next_loop.edge)
                         next_loop = next_loop.link_loop_next.link_loop_radial_next.link_loop_next
-                        if len(next_loop.vert.link_edges) != 4: # comes to mesh cut
+                        if len(next_loop.vert.link_edges) != 4:     # comes to mesh cut
                             break
-        # bm.to_mesh(mesh.data)
-        bm.free()
         return loops
 
     @staticmethod
-    def get_perpendicular_loops(mesh):
-        bpy.ops.object.mode_set(mode='OBJECT')
-
+    def get_parallel_loops(bm):
         loops = []
-        bm = bmesh.new()
-        bm.from_mesh(mesh.data)
-
         bm.edges.ensure_lookup_table()
         # selected edges - only with same count of edges on extreme verts (prevent if edge comes to mesh cut (from 4 verts to 3 in one edge)
-        selected_edges = [edge for edge in bm.edges if edge.select and len(edge.verts[0].link_loops) == len(edge.verts[1].link_loops)]
+        # selected_edges = [edge for edge in bm.edges if edge.select and len(edge.verts[0].link_loops) == len(edge.verts[1].link_loops)]
+        selected_edges = [edge for edge in bm.edges if edge.select]
         for edge in selected_edges:
             edge.tag = True
         if selected_edges:
             # start selection (use only part not connected to mesh cut)
-            loops.append([edge.index for edge in selected_edges if len(edge.verts[0].link_loops) == len(edge.verts[1].link_loops) == 4])
+            # loops.append([edge.index for edge in selected_edges if len(edge.verts[0].link_loops) == len(edge.verts[1].link_loops) == 4])
+            loops.append([edge.index for edge in selected_edges])
             # from extreme edge comes by link_loop_next.link_loop_next and so on
             curr_edge = __class__.get_extreme_edge(selected_edges)
             next_edge = curr_edge.link_loops[0].link_loop_next.link_loop_next.edge
             if len(next_edge.verts[0].link_loops) != 4 or len(next_edge.verts[1].link_loops) != 4:
                 next_edge = curr_edge.link_loops[1].link_loop_next.link_loop_next.edge
             while next_edge:
-                if next_edge.tag or len(next_edge.verts[0].link_loops) != 4 or len(next_edge.verts[1].link_loops) != 4:   # closed or comes to mesh cut
+                # if next_edge.tag or len(next_edge.verts[0].link_loops) != 4 or len(next_edge.verts[1].link_loops) != 4:   # closed or comes to mesh cut
+                if next_edge.tag:   # closed or comes to mesh cut
                     break
                 # get all edges from current edge (next_edge) - make loop checking parallel selection
                 next_loop = [next_edge.index]
                 loops.append(next_loop)
                 next_edge_loop = next_edge.link_loops[0].link_loop_next.link_loop_radial_next.link_loop_next
-                if not next_edge_loop.link_loop_next.link_loop_next.edge.tag and not next_edge_loop.link_loop_radial_next.link_loop_next.link_loop_next.edge.tag:
+                if not next_edge_loop.link_loop_next.link_loop_next.edge.tag and not next_edge_loop.link_loop_radial_next.link_loop_next.link_loop_next.edge.tag and len(next_edge.link_loops) > 1:
                     next_edge_loop = next_edge.link_loops[1].link_loop_next.link_loop_radial_next.link_loop_next
                 if not next_edge_loop.link_loop_next.link_loop_next.edge.tag and not next_edge_loop.link_loop_radial_next.link_loop_next.link_loop_next.edge.tag:
                     next_edge_loop = None
@@ -115,6 +109,8 @@ class Retuber:
                     next_edge_loop = next_edge_loop.link_loop_next.link_loop_radial_next.link_loop_next
                     if not next_edge_loop.link_loop_next.link_loop_next.edge.tag and not next_edge_loop.link_loop_radial_next.link_loop_next.link_loop_next.edge.tag:
                         next_edge_loop = None
+                    if next_edge_loop and len(next_edge_loop.vert.link_edges) != 4:  # comes to mesh cut
+                        break
                 next_edge.tag = True
                 # get next edge
                 if len(next_edge.link_loops) == 1:  # comes to mesh cut
@@ -122,12 +118,11 @@ class Retuber:
                 else:
                     next_edge_tmp = next_edge.link_loops[0].link_loop_next.link_loop_next.edge
                     # if mesh cut or already selected - try opposite direction
-                    if next_edge_tmp.tag or len(next_edge_tmp.verts[0].link_loops) != 4 or len(next_edge_tmp.verts[1].link_loops) != 4:
+                    # if next_edge_tmp.tag or len(next_edge_tmp.verts[0].link_loops) != 4 or len(next_edge_tmp.verts[1].link_loops) != 4:
+                    if next_edge_tmp.tag:
                         next_edge = next_edge.link_loops[1].link_loop_next.link_loop_next.edge
                     else:
                         next_edge = next_edge_tmp
-        # bm.to_mesh(mesh.data)
-        bm.free()
         return loops
 
     @staticmethod
@@ -135,7 +130,6 @@ class Retuber:
         # return any extreme edge from edge list
         rez = None
         for edge in edge_list:
-            # if edge.link_loops[0].link_loop_next.link_loop_radial_next.link_loop_next.edge.tag == True:
             l1 = len([edge for edge in edge.verts[0].link_edges if edge.tag])
             l2 = len([edge for edge in edge.verts[1].link_edges if edge.tag])
             if l1 == 1 or l2 == 1:
@@ -174,6 +168,14 @@ class Retuber:
                     selected_verts.append(curr_vert)
         return selected_verts
 
+    @staticmethod
+    def save_environment(context):
+        __class__.environment['mesh_select_mode'] = context.tool_settings.mesh_select_mode[:]
+
+    @staticmethod
+    def restore_environment(context):
+        context.tool_settings.mesh_select_mode = __class__.environment['mesh_select_mode']
+
 
 class RetuberPanel(bpy.types.Panel):
     bl_idname = 'retuber.panel'
@@ -183,49 +185,144 @@ class RetuberPanel(bpy.types.Panel):
     bl_category = '1D'
 
     def draw(self, context):
-        button = self.layout.operator('retuber.mesh', icon='NONE', text='Perpendicular')
+        self.layout.operator('retuber.start', icon='NONE', text='Start retube')
+        # test
+        self.layout.label(text='Select only:')
+        row = self.layout.row()
+        button = row.operator('retuber.test', icon='NONE', text='Paralel')
         button.mode = True
-        button = self.layout.operator('retuber.mesh', icon='NONE', text='Parallel')
+        button = row.operator('retuber.test', icon='NONE', text='Perpendicular')
         button.mode = False
 
 
-class RetuberMesh(bpy.types.Operator):
-    bl_idname = 'retuber.mesh'
-    bl_label = 'Retuber mesh'
+class RetuberMeshTest(bpy.types.Operator):
+    bl_idname = 'retuber.test'
+    bl_label = 'Test'
     bl_options = {'REGISTER', 'UNDO'}
 
     mode = bpy.props.BoolProperty(
-        default=True
+        default=False
     )
 
     def execute(self, context):
 
-        mesh = context.object
+        bpy.ops.object.mode_set(mode='OBJECT')
+        bm = bmesh.new()
+        bm.from_mesh(context.object.data)
+        para_loops = Retuber.get_parallel_loops(bm)
+        bm.from_mesh(context.object.data)
+        perp_loops = Retuber.get_perpendicular_loops(bm)
+        bm.free()
 
-        if self.mode:
-            lst = Retuber.get_parallel_loops(mesh)
-        else:
-            lst = Retuber.get_perpendicular_loops(mesh)
-
-        # bpy.ops.object.select_all(action='DESELECT')
         bpy.ops.object.mode_set(mode='EDIT')
         bpy.ops.mesh.select_all(action='DESELECT')
+
         bpy.ops.object.mode_set(mode='OBJECT')
-
-        # print('all loops')
-        # print(lst)
-
-        for i in range(0, len(lst), 1):
-            for edge_index in lst[i]:
-                mesh.data.edges[edge_index].select = True
-
+        if self.mode:
+            print('para select')
+            print('para_loops', para_loops)
+            # select para loops
+            for loop in para_loops:
+                for edge_index in loop:
+                    context.object.data.edges[edge_index].select = True
+        else:
+            print('perp select')
+            print('perp_loops', perp_loops)
+            # select pert loops
+            for loop in perp_loops:
+                for edge_index in loop:
+                    context.object.data.edges[edge_index].select = True
         bpy.ops.object.mode_set(mode='EDIT')
-
-
         return {'FINISHED'}
 
 
+class RetuberMesh(bpy.types.Operator):
+    bl_idname = 'retuber.start'
+    bl_label = 'Start retuber for work'
+    bl_options = {'REGISTER', 'UNDO'}
+
+    start_mesh_data = None
+    para_loops = []
+    para_iteration = 0
+    perp_loops = []
+    perp_iteration = 0
+
+    def execute(self, context):
+        Retuber.save_environment(context)
+        context.tool_settings.mesh_select_mode = (False, True, False)   # select edges mode
+        # get parallel/perpenducular loops
+        if context.active_object.mode != 'OBJECT':
+            bpy.ops.object.mode_set(mode='OBJECT')
+        bm = bmesh.new()
+        bm.from_mesh(context.object.data)
+        self.para_loops = Retuber.get_parallel_loops(bm)
+        bm.from_mesh(context.object.data)
+        self.perp_loops = Retuber.get_perpendicular_loops(bm)
+        bm.free()
+        # cave mesh data to work with
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.select_all(action='DESELECT')
+        self.start_mesh_data = context.object.data.copy()
+        # switch to modal mode to interactive dissolving loops
+        context.window_manager.modal_handler_add(self)
+        return {'RUNNING_MODAL'}
+
+    def modal(self, context, event):
+        if event.type in ['WHEELUPMOUSE', 'WHEELDOWNMOUSE'] and (event.ctrl or event.shift):
+            perp_loops_to_remove = []
+            para_loops_to_remove = []
+            if event.type == 'WHEELUPMOUSE':
+                if event.shift:
+                    self.perp_iteration -= 1
+                    if self.perp_iteration < 0:
+                        self.perp_iteration = 0
+                elif event.ctrl:
+                    self.para_iteration -= 1
+                    if self.para_iteration < 0:
+                        self.para_iteration = 0
+            elif event.type == 'WHEELDOWNMOUSE':
+                if event.shift:
+                    self.perp_iteration += 1
+                elif event.ctrl:
+                    self.para_iteration += 1
+            # dissolve edges
+            # get edges to dissolve
+            perp_loops = self.perp_loops[:]
+            for i in range(self.perp_iteration):
+                perp_loops_to_remove += perp_loops[::2]
+                perp_loops = perp_loops[1::2]
+            para_loops = self.para_loops[:]
+            for i in range(self.para_iteration):
+                para_loops_to_remove += para_loops[::2]
+                para_loops = para_loops[1::2]
+            # dissolve
+            if context.object.mode != 'OBJECT':
+                bpy.ops.object.mode_set(mode='OBJECT')
+            context.object.data = self.start_mesh_data.copy()
+            bpy.ops.object.mode_set(mode='EDIT')
+            bpy.ops.mesh.select_all(action='DESELECT')
+            # dissolve perp loops
+            bpy.ops.object.mode_set(mode='OBJECT')
+            for loop in perp_loops_to_remove:
+                for edge_index in loop:
+                    context.object.data.edges[edge_index].select = True
+            bpy.ops.object.mode_set(mode='EDIT')
+            # bpy.ops.mesh.dissolve_edges()
+            # dissolve para loops
+            bpy.ops.object.mode_set(mode='OBJECT')
+            for loop in para_loops_to_remove:
+                for edge_index in loop:
+                    context.object.data.edges[edge_index].select = True
+            bpy.ops.object.mode_set(mode='EDIT')
+            bpy.ops.mesh.dissolve_edges()
+        elif event.type in ('RET', 'NUMPAD_ENTER', 'ESC'):
+            Retuber.restore_environment(context)
+            return {'FINISHED'}
+        return {'INTERFACE'}
+
+
 def register():
+    bpy.utils.register_class(RetuberMeshTest)
     bpy.utils.register_class(RetuberMesh)
     bpy.utils.register_class(RetuberPanel)
 
@@ -233,6 +330,7 @@ def register():
 def unregister():
     bpy.utils.unregister_class(RetuberPanel)
     bpy.utils.unregister_class(RetuberMesh)
+    bpy.utils.unregister_class(RetuberMeshTest)
 
 
 if __name__ == '__main__':
